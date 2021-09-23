ca88cb62cb162ffeb47459674bca73739dc1e860
조은혜 <jovely8040@gmail.com>
2021-09-23 11:47:06 +0900

    First Component

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './components/Item';
 
+function App() {
+  return (
+    <div className="App">
+      <Item name="Notebook"></Item>
+      <Item name="Smartphone"></Item>
+      <Item name="Tablet"></Item>
+      <Item name="Monitor"></Item>
+    </div>
+  )
+}
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          첫 번째 리액트 입니다.
+          첫 번째 리액트 앱입니다.
         </p>
         <a
           className="App-link"
@@ -21,5 +33,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;

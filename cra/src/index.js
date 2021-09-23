1c127fb6da20145f3db556c3b27cc58f5136b80b
조은혜 <jovely8040@gmail.com>
2021-09-23 15:20:14 +0900

    컴포넌트에서 state 사용하기

--- cra/src/index.js
+++ cra/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import App from './App.props'; // 루트 컴포넌트 렌더링
+// import App from './App.props'; // 루트 컴포넌트 렌더링. Props 테스트
+import App from './App.state';    //  State 테스트
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
